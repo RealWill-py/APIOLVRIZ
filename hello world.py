1aa7c53d2f0b5ff74de2ccacabba0ae7a5233f1c
Willian Oliveira <37823669+RealWill-py@users.noreply.github.com>
2023-06-27 07:33:16 -0300

    Update hello world.py
    ;)

--- hello world.py
+++ hello world.py
@@ -1,1 +1,19 @@
-print("hello world")
+import tkinter as tk
+
+def say_hello():
+    label["text"] = "Hello, World!"
+
+# Criação da janela
+window = tk.Tk()
+window.title("Hello, World!")
+
+# Criação do rótulo
+label = tk.Label(window, text="Clique no botão para exibir a mensagem.")
+label.pack(pady=10)
+
+# Criação do botão
+button = tk.Button(window, text="Clique aqui!", command=say_hello)
+button.pack(pady=5)
+
+# Execução da janela
+window.mainloop()
